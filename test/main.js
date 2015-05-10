7af646005cde72d0f1bfb4d5a6680ad9ea3a737f
Roel Dev <roeldev@users.noreply.github.com>
2015-05-10 21:29:00 +0200

    New util functions + tests

--- test/main.js
+++ test/main.js
@@ -1,11 +1,14 @@
 /**
  * log-interceptor | test/main.js
- * file version: 0.00.002
+ * file version: 0.00.003
  */
 'use strict';
 
 var Assert         = require('assert');
+var Chalk          = require('chalk');
+// var Level          = require('../lib/level.js');
 var LogInterceptor = require('../lib/index.js');
+var Utils          = LogInterceptor.utils;
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -116,6 +119,95 @@ describe('LogInterceptor.endAll()', function logInterceptorEndAllTests()
     it('should return false when no sessions to end', function()
     {
         Assert.strictEqual(LogInterceptor.endAll(), false);
+    });
+});
+
+describe('Utils.stripColor()', function utilsStripColorTests()
+{
+    it('should strip the colors', function()
+    {
+        var $input = Chalk.red('test');
+
+        Assert.strictEqual(Utils.stripColor($input), 'test');
+    });
+
+    it('should return the exact same string', function()
+    {
+        var $input = 'test';
+
+        Assert.strictEqual(Utils.stripColor($input), $input);
+    });
+});
+
+describe('Utils.trimTimestamp()', function utilsTrimTimestampTests()
+{
+    it('should trim the timestamp', function()
+    {
+        var $input = 'test timestamp';
+
+        Assert.strictEqual(Utils.trimTimestamp('[00:00:00] ' + $input), $input);
+    });
+
+    it('should return the exact same string [1]', function()
+    {
+        var $input = 'test';
+
+        Assert.strictEqual(Utils.trimTimestamp($input), $input);
+    });
+
+    it('should return the exact same string [2]', function()
+    {
+        var $input = '[23:60:00] test';
+
+        Assert.strictEqual(Utils.trimTimestamp($input), $input);
+    });
+});
+
+describe('Utils.trimLinebreak()', function utilsTrimLinebreakTests()
+{
+    it('should trim the linebreak at the end [1]', function()
+    {
+        var $input = 'test tets';
+
+        Assert.strictEqual(Utils.trimLinebreak($input + '\n'), $input);
+    });
+
+    it('should trim the linebreak at the end [2]', function()
+    {
+        var $input = 'test\ntets\n';
+
+        Assert.strictEqual(Utils.trimLinebreak($input + '\n'), $input);
+    });
+
+    it('should return the exact same string [1]', function()
+    {
+        var $input = 'test tets';
+
+        Assert.strictEqual(Utils.trimLinebreak($input), $input);
+    });
+
+    it('should return the exact same string [2]', function()
+    {
+        var $input = 'test\ntets';
+
+        Assert.strictEqual(Utils.trimLinebreak($input), $input);
+    });
+});
+
+describe('Utils.splitOnLinebreak()', function utilsSplitOnLinebreakTests()
+{
+    it('should split the string and return an array', function()
+    {
+        var $input = 'test1\ntest2\n';
+
+        Assert.deepEqual(Utils.splitOnLinebreak($input), $input.split('\n'));
+    });
+
+    it('should return an array with one value', function()
+    {
+        var $input = 'test tets';
+
+        Assert.deepEqual(Utils.splitOnLinebreak($input), [$input]);
     });
 });
 
